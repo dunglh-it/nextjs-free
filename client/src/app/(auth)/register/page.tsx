0eda554d728127e3eed4d38f60538218ba5917d3
dunglh-it <dunglh.it@gmail.com>
2024-10-31 22:18:44 +0700

    Init

---
+++ client/src/app/(auth)/register/page.tsx
@@ -0,0 +1,12 @@
+import { ModeToggle } from '@/components/mode-toggle'
+
+const RegisterPage = () => {
+  return (
+    <div>
+      <ModeToggle />
+      Register page
+    </div>
+  )
+}
+
+export default RegisterPage
