1913c28799a1a40f38fd79948bb855b8e2514733
dunglh-it <dunglh.it@gmail.com>
2024-11-01 23:14:28 +0700

    Code trang Login

--- client/src/app/(auth)/login/page.tsx
+++ client/src/app/(auth)/login/page.tsx
@@ -1,6 +1,15 @@
 // 'use client'
 
+import LoginForm from '@/app/(auth)/login/login-form'
+
 export default function LoginPage() {
   // const [email, setEmail] = useState('duthanhduoc@gmail.com')
-  return <div>Login page</div>;
+  return (
+    <div>
+      <h1 className="text-xl font-semibold text-center">Đăng nhập</h1>
+      <div className="flex justify-center">
+        <LoginForm />
+      </div>
+    </div>
+  )
 }
